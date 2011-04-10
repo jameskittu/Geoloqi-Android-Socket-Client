c3beb0cd0deaa541caf7ec222242ff9e32e7946a
Aaron Parecki <aaron@parecki.com>
2011-04-10 14:05:57 -0700

    Reading from the socket now happens on a background thread! Moves access token into a .gitignore'd file.

---
+++ src/com/geoloqi/socket/GeoloqiConstantsTemplate.java
@@ -0,0 +1,5 @@
+package com.geoloqi.socket;
+
+public class GeoloqiConstantsTemplate {
+	public static final String ACCESS_TOKEN = "Get your access token at http://beta.geoloqi.com/settings/connections";
+}
--- src/com/geoloqi/socket/geoloqiSocketClient.java
+++ src/com/geoloqi/socket/geoloqiSocketClient.java
@@ -1,6 +1,7 @@
 package com.geoloqi.socket;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -10,22 +11,26 @@ import java.net.Socket;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
-public class geoloqiSocketClient extends Activity {
+public class GeoloqiSocketClient extends Activity {
 	static String TAG = "geoloqi.socket";
 	static String host = "api.geoloqi.com";
 	static int port = 40000;
-	static String accessToken = "";
+	
+	public InputStream in;
+	public TextView textView;
 
 	String readFromSocketInputStream(InputStream is) throws Exception
 	{
 		// Create a byte array to store the number of bytes coming up
 		byte[] b = new byte[4];
-		Log.i(TAG, "waiting for input from stream");
+		Log.i(TAG, "waiting for message length packet from stream");
 		int numread = is.read(b, 0, 4);
 		if(numread != 4) {
 			throw new Exception("Wrong number of bytes were read: " + numread + " expecting 4");
@@ -35,8 +40,8 @@ public class geoloqiSocketClient extends Activity {
 					  | ((int)(b[1] & 0xFF) << 8) 
 					  | (int)b[0] & 0xFF;
 
-		byte[] buffer = new byte[numBytes];  // TODO: How to do this properly? Maybe we need a buffered reader?
-		Log.i(TAG, "attempting to read " + numBytes + " from socket");
+		byte[] buffer = new byte[numBytes];  // TODO: How do we handle reading large chunks of data?
+		Log.i(TAG, "attempting to read " + numBytes + " bytes from socket");
 		numread = is.read(buffer, 0, numBytes);
 		
 		if(numread != numBytes) {
@@ -44,6 +49,7 @@ public class geoloqiSocketClient extends Activity {
 		}
 		
 		String str = new String(buffer, 0, numBytes);
+		Log.i(TAG, "read message: '" + str + "'");
 		return str;
 	}
 
@@ -53,39 +59,67 @@ public class geoloqiSocketClient extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		TextView text = (TextView) findViewById(R.id.textView1); 
-		text.setText("Android yay.");
+		textView = (TextView)findViewById(R.id.textView1); 
+		textView.setText("Connecting...\n");
 
 		try
 		{
 			Socket s = new Socket(host, port);
 			OutputStream out = s.getOutputStream();
-			InputStream in = s.getInputStream();
+			in = s.getInputStream();
+
+			// doing this stuff on the main thread for now, move it later, possibly using the "tag" scheme from the iPhone version
 			
 			// connect and get the access token prompt
 			String response = readFromSocketInputStream(in);
 
 			// got the prompt, send the access token
-			out.write(accessToken.getBytes());
+			out.write(GeoloqiConstants.ACCESS_TOKEN.getBytes());
 			out.flush();
 
 			// read the "logged in" prompt
 			response = readFromSocketInputStream(in);
 			Log.i(TAG, response);
-			
-			// Wait for more data from the socket and output it
-			while(true) {
-				response = readFromSocketInputStream(in);
-				text.setText(response);
-				Log.i(TAG, response);
-			}
+			textView.append(response);
 
+			// Start listening to the socket for data
+			Thread readerThread = new Thread(new IncomingReader());
+			readerThread.start();
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
-			text.setText("Error: " + e.getMessage());
+			textView.append("Error: " + e.getMessage() + "\n");
 		}
 
 	}
+	
+	final Handler incomingHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			textView.append((String)msg.obj + "\n");
+			super.handleMessage(msg);
+		}
+	};
+	
+	public class IncomingReader implements Runnable {
+		
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				while(true) {
+					String response = readFromSocketInputStream(in);
+					Message msg = incomingHandler.obtainMessage();
+					msg.obj = response;
+					incomingHandler.sendMessage(msg);
+					Log.i(TAG, "Got this in the background: " + response);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
 }
